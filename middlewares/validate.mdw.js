3e51349a0c246294df6ba20c7006fae8695c7c60
hoangnguyen <hoangg.nguyen28@gmail.com>
2021-07-26 14:07:55 +0700

    feat:validate actor

---
+++ middlewares/validate.mdw.js
@@ -0,0 +1,13 @@
+const ajv_lib = require("ajv")
+
+module.exports = function (schema) {
+    return function (req, res, next) {
+        const ajv = new ajv_lib({ allErrors: true }) // options can be passed, e.g. {allErrors: true}
+        const validate = ajv.compile(schema)
+        const valid = validate(req.body)
+        if (!valid) {
+            return res.status(400).json(validate.errors)
+        }
+        next()
+    }
+}
